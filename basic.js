8a901d24f0814db69e0d63f5d9978a6bf4667068
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-27 11:01:15 +0600

    comment something

--- basic.js
+++ basic.js
@@ -1,13 +1,13 @@
 const numbers = [23, 34, 54, 65, 76];
-const numberCount = numbers.length;
+const numberCount = numbers.length; // check Length
+numbers.pop(); // remove last element
 numbers.pop();
 numbers.pop();
-numbers.pop();
+numbers.push(344) // add last new alement
 numbers.push(344)
 numbers.push(344)
-numbers.push(344)
-numbers.shift()
-numbers.unshift(3454)
-numbers[2] = 888;
+numbers.shift() // remove first element
+numbers.unshift(3454) // add new first element
+numbers[2] = 888; // change element
 numbers[4] = 9999;
 console.log(numbers);
