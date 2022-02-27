925d05cddd46f5bb4d19137963df9d195ad236ae
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-27 10:49:22 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,13 @@
+const numbers = [23, 34, 54, 65, 76];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.pop();
+numbers.pop();
+numbers.push(344)
+numbers.push(344)
+numbers.push(344)
+numbers.shift()
+numbers.unshift(3454)
+numbers[2] = 888;
+numbers[4] = 9999;
+console.log(numbers);
